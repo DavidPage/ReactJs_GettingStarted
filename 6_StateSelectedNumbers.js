2d24007d18cf376a3ce66024e61d82b2c6a91032
David Cuevas <davidjfcpage@gmail.com>
2017-07-02 19:33:25 +0100

    Added selected or deselected number actions. Button improved

---
+++ 6_StateSelectedNumbers.js
@@ -0,0 +1,109 @@
+//uses font awesome to print stars
+const Stars = (props) => {
+
+  const numberOfStars =  1 + Math.floor(Math.random()*9);
+
+  let stars = [];
+
+
+  return(
+    <div className="col-5">
+      {_.range(numberOfStars).map(i =>   <i key={i} className="fa fa-star" />)}
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return(
+    <div className="col-2">
+      <button> = </button>
+  </div>
+);
+}
+
+const Answer = (props) => {
+  return(
+    <div className="col-5">
+      {props.selectedNumbers.map((number,i) =>
+      	<span key={i}>{number}</span>
+      )}
+    </div>
+  );
+}
+
+const Numbers = (props) => {
+
+	consts numberClassName = (number) => {
+  	if(props.selectedNumbers.indexOF(number) >= 0){
+    return 'selected';
+    }
+  }
+
+  //lodash library to generate this array
+  //  const arrayOfNumbers = _.range(1,10);
+  return(
+    <div className="card text-center">
+      <div>
+        {
+          Numbers.list.map((number, i) =>
+          <span key={i} className={numberClassName(number)}>
+            {number}
+          </span>
+        )
+      }
+    </div>
+  </div>
+);
+}
+
+//In order to avoid this array to be generated everysingle time a Numbers
+//component is rendered, we should bring it outside the component and then
+//access it from the components that might use it
+Numbers.list = _.range(1,10);
+
+
+class Game extends React.Component{
+
+state = {
+	selectedNumbers: [2,4]
+};
+
+selectNumber = (clickedNumber) => {
+this.setState(prevState => ({
+	selectedNUmbers: prevState.selectedNumbers.concat(clickedNumber)
+}));
+};
+  render (){
+    return(
+      <div className="container">
+        <h3>
+          Play nine
+        </h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer selectedNumbers={this.state.selectedNumbers}/>
+        </div>
+        <br />
+        <Numbers selectedNumbers={this.state.selectedNumbers} />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component{
+  render (){
+    return(
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <App />,
+  mountNode
+);
